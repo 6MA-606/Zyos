17277c2707782202ef94c398de51fe915a95e29d
6MA-606 <zyxma.6666@hotmail.com>
2024-08-29 23:22:49 +0700

    Update 1.1.0

--- index.js
+++ index.js
@@ -1,16 +1,75 @@
-import { ZyosResponse } from "./libs/ZyosResponse"
 
-// Zyos Configuration
-const config = {
-  defaultTokenKey: 'Authorization',
-  defaultToken: null,
-  getToken: () => {
-    return localStorage.getItem('itbkk-token')
-  },
-  defaultHeaders: {
-    'Content-Type': 'application/json',
-  },
-  defaultMethod: 'GET',
+class ZyosConfig {
+
+  /**
+   * Create a new ZyosConfig object
+   */
+  constructor() {
+    this.defaultToken = null
+    this.defaultTokenGetter = null
+    this.defaultHeaders = {
+      'Content-Type': 'application/json',
+    }
+    this.defaultMethod = 'GET'
+  }
+}
+
+class ZyosResponse {
+  
+  /**
+   * Create a new ZyosResponse object
+   * @param {string} status - Status of the response
+   * @param {string} message - Message of the response
+   * @param {object} data - Data of the response
+   * @param {number} statusCode - Status code of the response
+  */
+  constructor(status, message, data, statusCode) {
+    this.status = status
+    this.message = message || null
+    this.data = data || null
+    this.statusCode = statusCode || null
+  }
+
+  /**
+   * Create a success response
+   * @param {string} data - Data to send in the response
+   * @param {number} statusCode - Status code of the response
+   * @returns 
+  */
+  static success(data, statusCode) {
+    return new ZyosResponse('success', null, data, statusCode)
+  }
+  
+  /**
+   * Create an error response
+   * @param {string} message - Error message to send in the response
+   * @param {number} statusCode - Status code of the response
+   * @returns 
+  */
+  static error(message, statusCode) {
+    return new ZyosResponse('error', message, null, statusCode)
+  }
+  
+  /**
+   * Compute the data of the response with a function and return the response that contains the computed data.
+   * @param {(data: object) => object} computeFunction - A function that receives the current data and returns the computed data.
+   * @returns {ZyosResponse} The updated response object.
+  */
+  compute(computeFunction) {
+    this.data = computeFunction(this.data)
+    return this
+  }
+}
+
+/** @type {ZyosConfig} */
+const config = new ZyosConfig()
+
+/**
+ * Define the Zyos configuration
+ * @param {ZyosConfig} userDefinedConfig - Configuration to define
+ */
+function defineConfig(userDefinedConfig) {
+  Object.assign(config, userDefinedConfig)
 }
 
 /**
@@ -23,12 +82,12 @@ const config = {
  * @param {boolean} options.useToken - Use token in the request
  * @param {string} options.token - Token to send in the request
  * @param {string} options.tokenKey - Key to send the token in the headers
- * @param {function} options.getToken - Function to get and return the token
+ * @param {function} options.tokenGetter - Function to get and return the token
  * @param {(data: object) => object} options.computeFunction - Function to compute the data of the response before returning it
- * @returns {ZyosResponse} The response of the fetch
- */
+ * @returns {Promise<ZyosResponse>} The response of the fetch
+*/
 async function fetch(url, options = {}) {
-  
+
   const headers = {
     ...config.defaultHeaders,
     ...options.headers
@@ -45,7 +104,7 @@ async function fetch(url, options = {}) {
   }
 
   if (options.useToken) {
-    const optionToken = options.token || options.getToken || config.defaultToken || config.getToken || null
+    const optionToken = options.token || options.tokenGetter || config.defaultToken || config.defaultTokenGetter || null
     const token = typeof optionToken === 'function' ? optionToken() : optionToken
     const tokenKey = options.tokenKey || config.defaultTokenKey
     if (token) {
@@ -58,7 +117,6 @@ async function fetch(url, options = {}) {
   delete fetchOptions.useToken
 
   try {
-    console.log(fetchOptions)
     const response = await window.fetch(url, fetchOptions)
     let data = await response.json()
 
@@ -76,6 +134,9 @@ async function fetch(url, options = {}) {
   }
 }
 
+export { ZyosResponse, ZyosConfig }
+
 export default {
-  fetch
+  fetch,
+  defineConfig
 }
